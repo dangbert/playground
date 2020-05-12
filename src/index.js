736d11c16deb2d36d0fb3cb06db862b7a6d9f056
Daniel Engbert <end1@umbc.edu>
2020-05-12 19:22:06 -0400

    Game class now manages all state (and stores history of squares).

--- src/index.js
+++ src/index.js
@@ -4,14 +4,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [
+                {squares: Array(9).fill(null)}
+            ],
+            xIsNext: true
+        }
+    }
+    handleClick(i) {
+        const history = this.state.history
+        const squares = history[history.length-1].squares.slice(); // returns a shallow copy?
+
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        history.push({"squares": squares})
+        // in React you can't change this.state directly:
+        this.setState({history: history, xIsNext: !this.state.xIsNext});
+    }
     render() {
+        const history = this.state.history
+        const squares = history[history.length-1].squares.slice();
+        const winner = calculateWinner(squares);
+        let status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        if (winner) {
+            status = "Winner: " + winner;
+        }
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                        onClick={(i) => this.handleClick(i)}
+                        squares={this.state.history[this.state.history.length-1].squares}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
+                    <div>{status}</div>
                     <ol>{/* TODO */}</ol>
                 </div>
             </div>
@@ -20,40 +51,17 @@ class Game extends React.Component {
 }
 
 class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true
-        }
-    }
-    handleClick(i) {
-        const squares = this.state.squares.slice(); // returns a shallow copy?
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        // in React you shouldn't change this.state directly:
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({squares: squares, xIsNext: !this.state.xIsNext});
-    }
     renderSquare(i) {
         return (
             <Square
-                value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
             />
         );
     }
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        if (winner) {
-            status = "Winner: " + winner;
-        }
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -97,14 +105,9 @@ ReactDOM.render(
  */
 function calculateWinner(squares) {
     const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6],
     ];
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
