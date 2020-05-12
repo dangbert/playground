4cf1fa9f117ff52b602b1878e54e3b0b01180bd3
Daniel Engbert <end1@umbc.edu>
2020-05-11 22:56:09 -0400

    Base version of game done. Now ready to start the "Adding Time Travel" step in the tutorial.

--- src/index.js
+++ src/index.js
@@ -28,8 +28,11 @@ class Board extends React.Component {
         }
     }
     handleClick(i) {
-        // in React you shouldn't change this.state directly:
         const squares = this.state.squares.slice(); // returns a shallow copy?
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        // in React you shouldn't change this.state directly:
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({squares: squares, xIsNext: !this.state.xIsNext});
     }
@@ -42,7 +45,11 @@ class Board extends React.Component {
         );
     }
     render() {
-        const status = 'Next player: X';
+        const winner = calculateWinner(this.state.squares);
+        let status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        if (winner) {
+            status = "Winner: " + winner;
+        }
 
         return (
             <div>
@@ -83,3 +90,27 @@ ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
+/*
+ * Given an array of 9 squares, this function will check for a winner and
+ * return 'X', 'O', or null as appropriate.
+ */
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
