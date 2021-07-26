a6a73b3f90a927e6f4922bf341c9ba7f8c51069b
Daniel Engbert <end1@umbc.edu>
2021-07-26 19:11:02 -0400

    Print some context around given result.

--- wikipedia/sentences.py
+++ wikipedia/sentences.py
@@ -10,20 +10,33 @@ import re
 #   https://dumps.wikimedia.org/ptwiki/20210720/
 path = './data' # name of file (after being extracted)
 
+def getExample(m, sIndex, eIndex):
+    """
+    return a string with additional context on either side of the given search result
+    """
+    sIndex = max(0, sIndex - 200)
+    eIndex = min(len(m) - 1, eIndex + 200)
+    return m[sIndex:eIndex].decode()
+
+target = rb'Daniel'
+
 with open(path,'r', encoding='UTF-8') as f:
     m = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ) # store file as bytes
 
     # https://pymotw.com/3/mmap/#regular-expressions
-    pattern = re.compile(rb'ferrocarril', re.IGNORECASE)
-    res = pattern.findall(m)
-    print("num results = {}".format(len(res)))
-    print("first result = ")
-    print(res[0])
+    pattern = re.compile(target, re.IGNORECASE)
 
-    exit(1)
+    count = 0
+    r = pattern.search(m)
+    while r and count < 10:
+        count += 1
+        text = m[r.start():r.end()].decode() 
+        print("\n\n({0}, {1}) ~ {2}".format(r.start(), r.end() - 1, text))
+        sample = getExample(m, r.start(), r.end() - 1)
+        print(sample)
+        r = pattern.search(m, r.start() + 1)
 
-    print('------')
-    # mapping = dict((line.strip().split(' ') for line in f if line))
-    mapping = [line.strip().split(' ') for line in f if line]
-    print(mapping[0])
+    #res = pattern.findall(m)
+    #print("num results = {}".format(len(res)))
+
     exit(1)
