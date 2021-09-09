a6ac1cb1eb69a12c75bc05fb724b54ab35f28a6b
Daniel Engbert <end1@umbc.edu>
2021-09-09 17:22:34 -0400

    proto buf example code.

--- protobuf/index.js
+++ protobuf/index.js
@@ -1,24 +1,67 @@
 const fs = require('fs');
+// yarn add google-protobuf
+
+/**
+ * example using protocol buffers
+ */
+const pBufExample = () => {
+  console.log('\n\n***running protocol buf example***');
+  const Schema = require('./employees_pb');
+  const dan = new Schema.Employee();
+  dan.setId(0);
+  dan.setName('Dan');
+  dan.setSalary(9001);
+  console.log(`created pbuf instace with name ${dan.getName()}`);
+
+  const bob = new Schema.Employee();
+  bob.setId(1);
+  bob.setName('Bob');
+  bob.setSalary(2000);
+
+  const employees = new Schema.Employees();
+  employees.addEmployees(dan);
+  employees.addEmployees(bob);
+
+  // now serialize employees to binary (for sending over network etc)
+  const bytes = employees.serializeBinary();
+  console.log('employees data as bytes:');
+  console.log(bytes);
+  fs.writeFileSync('data.binary', bytes);
+  console.log('wrote file data.binary');
+
+  // deserialize:
+  const employees2 = Schema.Employees.deserializeBinary(bytes);
+  console.log('deserialized:');
+  console.log(employees2.toString());
+};
+
+/**
+ * example using json instead.
+ */
+const jsonExample = () => {
+  console.log('\n\n***running json example***');
+  const employees = []
+
+  const dan = {
+    name: 'Dan',
+    salary: 9001,
+    id: 0,
+  }
+
+  const bob = {
+    name: 'Bob',
+    salary: 2000,
+    id: 1,
+  }
+
+  employees.push(dan);
+  employees.push(bob);
+
+  console.log(employees);
+  fs.writeFileSync('data.json', JSON.stringify(employees));
+  console.log('wrote file data.json');
+};
 
 
-const employees = []
-
-const dan = {
-  name: 'Dan',
-  salary: 1000,
-  id: 0,
-}
-
-const bob = {
-  name: 'Bob',
-  salary: 2000,
-  id: 1,
-}
-
-employees.push(dan);
-employees.push(bob);
-
-
-console.log(employees);
-
-fs.writeFileSync('data.json', JSON.stringify(employees));
+pBufExample();
+jsonExample();
