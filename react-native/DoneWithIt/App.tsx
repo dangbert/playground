e54edf020732ff417e6b378669b802bb0f0c74bc
Daniel Engbert <end1@umbc.edu>
2022-09-11 14:25:05 +0200

    Configure debugging, add webpack for react-native-web.

--- react-native/DoneWithIt/App.tsx
+++ react-native/DoneWithIt/App.tsx
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('hello');
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello Dan!</Text>
       <StatusBar style="auto" />
     </View>
   );
