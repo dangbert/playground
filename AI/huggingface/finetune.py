fc8133937f93945d3840567cfdd2bba43647ac3f
Daniel Engbert <end1@umbc.edu>
2024-03-09 16:35:15 +0100

    finetune.py: pytorch working as well

--- AI/huggingface/finetune.py
+++ AI/huggingface/finetune.py
@@ -6,15 +6,19 @@ from transformers import (
     AdamW,
     AutoModelForSequenceClassification,
     AutoTokenizer,
+    Trainer,
+    TrainingArguments,
+    DataCollatorWithPadding,
     get_scheduler,
 )
-from transformers import Trainer, TrainingArguments, DataCollatorWithPadding
 import torch
 from torch.utils.data import DataLoader
 import datasets
 import evaluate  # pip install evaluate scikit-learn
 import numpy as np
 import time
+from tqdm.auto import tqdm
+import wandb
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -42,7 +46,14 @@ def main():
         "-m",
         type=str,
         help=f"Method to fine tune with (one of {','.join(list(METHODS.keys()))})",
-        default=METHODS["trainer"],
+        default="pytorch",
+    )
+    parser.add_argument(
+        "--num-epochs",
+        "-n",
+        type=int,
+        help="number of epochs to train for",
+        default=4,
     )
     parser.add_argument(
         "--wandb",
@@ -51,35 +62,39 @@ def main():
         help=f"Enable wandb logging",
     )
 
-    ops = 0  # num operations performed
     args = parser.parse_args()
 
     if not args.wandb:
         os.environ["WANDB_MODE"] = "offline"  # don't sync
     else:
         os.environ["WANDB_MODE"] = "online"
-    # os.environ["WANDB_ENTITY"] = "dan-thesis"
-    # os.environ["WANDB_PROJECT"] = "playground"
 
     model = AutoModelForSequenceClassification.from_pretrained(CHECKPOINT, num_labels=2)
     tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT)
 
-    print(f"starting finetuning with method '{args.method}'")
+    print(
+        f"\nstarting finetuning with method '{args.method}' for {args.num_epochs} epochs"
+    )
     start_time = time.perf_counter()
-    METHODS[args.method(model, tokenizer, args.output_dir)]
+    METHODS[args.method](model, tokenizer, args)
     end_time = time.perf_counter()
-    print(f"finetuning completed in {((end_time - start_time) / 60):.2f} minutes")
+    print(f"finetuning complete in {((end_time - start_time) / 60):.2f} minutes!")
 
 
-def finetune_trainer(model, tokenizer, output_dir: str):
-    """Finetune using huggingface.Trainer class."""
+def finetune_trainer(model, tokenizer, args):
+    """
+    Finetune using huggingface.Trainer class.
+    Automatically uses wandb if os.environ["WANDB_MODE"] != "offline"
+    https://huggingface.co/learn/nlp-course/chapter3/3
+    """
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-    tokenized_datasets = get_datasets(tokenizer)
+    tokenized_datasets = get_datasets(tokenizer, for_pytorch=False)
 
     training_args = TrainingArguments(
-        output_dir,
+        args.output_dir,
         # evaluate every epoch
         evaluation_strategy="epoch",
+        num_train_epochs=args.num_epochs,
     )
     trainer = Trainer(
         model,
@@ -88,16 +103,24 @@ def finetune_trainer(model, tokenizer, output_dir: str):
         eval_dataset=tokenized_datasets["validation"],
         data_collator=data_collator,
         tokenizer=tokenizer,
-        compute_metrics=compute_metrics,
+        compute_metrics=compute_metrics_trainer,
     )
     print(f"\nfinetuning with Trainer...", flush=True)
     trainer.train()
 
+    model.save_pretrained(args.output_dir)
+    print(f"model saved to '{args.output_dir}'")
 
-def finetune_pytorch(model, tokenizer, output_dir: str):
-    """Finetune using pytorch directly."""
+
+def finetune_pytorch(model, tokenizer, args):
+    """
+    Finetune using pytorch directly.
+    https://huggingface.co/learn/nlp-course/chapter3/4
+    Note: this could be tweaked to use accelerate to enable training on multiple GPUs:
+    https://huggingface.co/learn/nlp-course/chapter3/4#supercharge-your-training-loop-with-accelerate
+    """
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-    tokenized_datasets = get_datasets(tokenizer)
+    tokenized_datasets = get_datasets(tokenizer, for_pytorch=True)
 
     train_dataloader = DataLoader(
         tokenized_datasets["train"],
@@ -111,11 +134,10 @@ def finetune_pytorch(model, tokenizer, output_dir: str):
 
     optimizer = AdamW(model.parameters(), lr=3e-5)
 
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    device = get_device()
     model.to(device)
 
-    num_epochs = 3
-    num_training_steps = num_epochs * len(train_dataloader)
+    num_training_steps = args.num_epochs * len(train_dataloader)
     lr_scheduler = get_scheduler(
         "linear",
         optimizer=optimizer,
@@ -125,21 +147,43 @@ def finetune_pytorch(model, tokenizer, output_dir: str):
 
     progress_bar = tqdm(range(num_training_steps))
 
-    model.train()
-    for epoch in range(num_epochs):
+    run = wandb.init(config=args)
+    for epoch in range(args.num_epochs):
+        model.train()
+        total_loss = 0.0
         for batch in train_dataloader:
             batch = {k: v.to(device) for k, v in batch.items()}
             outputs = model(**batch)
             loss = outputs.loss
             loss.backward()
+            total_loss += loss
 
             optimizer.step()
             lr_scheduler.step()
             optimizer.zero_grad()
             progress_bar.update(1)
 
+        eval_metrics = compute_metrics_pytorch(model, eval_dataloader)
+        train_metrics = {}
+        if epoch % 4 == 0 or epoch + 1 == args.num_epochs:
+            # expensive computation:
+            train_metrics = compute_metrics_pytorch(model, train_dataloader)
 
-def get_datasets(tokenizer):
+        wandb_metrics = {
+            "train_loss": total_loss,
+            **{f"{name}/eval": value for name, value in eval_metrics.items()},
+            **{f"{name}/train": value for name, value in train_metrics.items()},
+        }
+        wandb.log(
+            wandb_metrics,
+            step=epoch,
+        )
+
+    print("\nfinal metrics:\n", wandb_metrics)
+    wandb.finish()
+
+
+def get_datasets(tokenizer, for_pytorch: bool):
     raw_datasets = datasets.load_dataset("glue", "mrpc")
 
     def tokenize_function(example):
@@ -147,14 +191,53 @@ def get_datasets(tokenizer):
         return tokenizer(example["sentence1"], example["sentence2"], truncation=True)
 
     tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+
+    # https://huggingface.co/learn/nlp-course/chapter3/4#prepare-for-training
+    if for_pytorch:
+        # ensure we only have columns the pytorch model can accept
+        tokenized_datasets = tokenized_datasets.remove_columns(
+            ["sentence1", "sentence2", "idx"]
+        )
+        tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
+        tokenized_datasets.set_format("torch")
     return tokenized_datasets
 
 
-def compute_metrics(eval_preds):
+def compute_metrics_trainer(eval_preds):
     metric = evaluate.load("glue", "mrpc")
     logits, labels = eval_preds
     predictions = np.argmax(logits, axis=-1)
     return metric.compute(predictions=predictions, references=labels)
+
+
+def compute_metrics_pytorch(model, dataloader):
+    metric = evaluate.load("glue", "mrpc")
+    model.eval()
+    for batch in dataloader:
+        batch = {k: v.to(model.device) for k, v in batch.items()}
+        with torch.no_grad():
+            outputs = model(**batch)
+        logits = outputs.logits
+        predictions = torch.argmax(logits, dim=-1)
+        metric.add_batch(predictions=predictions, references=batch["labels"])
+
+    return metric.compute()
+
+
+def get_device() -> str:
+    """Returns the device for PyTorch to use."""
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+    # mac MPS support: https://pytorch.org/docs/stable/notes/mps.html
+    elif torch.backends.mps.is_available():
+        if not torch.backends.mps.is_built():
+            print(
+                "MPS not available because the current PyTorch install was not built with MPS enabled."
+            )
+        else:
+            device = "mps"
+    return device
 
 
 if __name__ == "__main__":
