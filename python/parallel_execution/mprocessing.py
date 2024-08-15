80c3a048b078c1ea755574fa72100165511986b2
Daniel Engbert <end1@umbc.edu>
2024-08-15 15:33:22 +0300

    parallel_excution: ruff format and fix

--- python/parallel_execution/mprocessing.py
+++ python/parallel_execution/mprocessing.py
@@ -5,39 +5,40 @@ https://www.youtube.com/watch?v=YOhrIov7PZA
 
 multiprocessing is helpful for CPU bound code.
 """
-from concurrent.futures import process
+
 from multiprocessing import Process, cpu_count, Array
 import time
 from typing import List
 
+
 def main():
-    print(f'note: this computer has {cpu_count()} cpus')
+    print(f"note: this computer has {cpu_count()} cpus")
     # count to 1 billion
-    #COUNT_TOTAL = pow(10, 9)
+    # COUNT_TOTAL = pow(10, 9)
     COUNT_TOTAL = pow(10, 8)
     start = time.perf_counter()
     NUM_PROCESSES = 4
 
     COUNT_EACH = int(COUNT_TOTAL / NUM_PROCESSES)
-    print(f'creating {NUM_PROCESSES} processes, each will count to {COUNT_EACH}')
+    print(f"creating {NUM_PROCESSES} processes, each will count to {COUNT_EACH}")
     plist: List[Process] = []
 
     # use a "shared variable" to let each thread store data
     #   https://docs.python.org/3/library/multiprocessing.html#sharing-state-between-processes
-    arr = Array('d', range(NUM_PROCESSES))
+    arr = Array("d", range(NUM_PROCESSES))
     for i in range(NUM_PROCESSES):
         plist.append(Process(target=counter, args=(COUNT_EACH, arr, i)))
         plist[-1].start()
 
-
     # have main process wait for processes to finish
-    print('\nwaiting on processes...')
+    print("\nwaiting on processes...")
     for p in plist:
         p.join()
     stop = time.perf_counter()
-    print(f'\nall done (in {(stop - start):.4f} secs)')
+    print(f"\nall done (in {(stop - start):.4f} secs)")
     print("cpu_times of each process = ")
     print(arr[:])
+
 
 def counter(num: int, arr: Array, arr_index: int):
     cpu_time = time.perf_counter()
