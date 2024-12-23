1b562afb1e7a8d6376f4e7ebf97fd1edc771eb7a
Daniel Engbert <end1@umbc.edu>
2024-12-23 10:12:27 -0500

    fastapi: add union experiment

--- python/fastapi/main.py
+++ python/fastapi/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi import Query
 from typing import Annotated
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -21,3 +22,17 @@ async def join(invite_code: Annotated[str, Query(min_length=10, max_length=10)])
             return {"course": course}
     # 400 error
     raise HTTPException(status_code=400, detail="Invalid invite code")
+
+
+class Item(BaseModel):
+    x: str | float | int
+
+
+@app.post("/test")
+async def test(item: Item):
+    return {"x": str(type(item.x))}
+
+
+# note these always were strings:
+# async def test(x: Union[int, float, str]):
+# async def test(x: int | float | str):
