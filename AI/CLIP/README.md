d43aa882e207a1cbd6eefe0187e7369a61eaa393
Daniel Engbert <end1@umbc.edu>
2025-02-10 23:21:46 +0000

    CLIP: query most similar image

---
+++ AI/CLIP/README.md
@@ -0,0 +1,11 @@
+
+On server with 4GB RAM and 2 CPUs, I encoded 419 images with model "RN50", with timing results:
+
+(Note I think only the first image had some cold start cost)
+
+````txt
+at image 0/418, averaging 0.8040 sec/image
+...
+at image 25/418, averaging 0.4525 sec/image
+at image 415/418, averaging 0.2991 sec/image
+````
--- AI/CLIP/go.py
+++ AI/CLIP/go.py
@@ -9,10 +9,13 @@ import torch
 import clip
 from PIL import Image
 import time
+import torch.nn.functional as F
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 SUPPORTED_EXTS = ["jpg", "jpeg", "png"]
 MODEL_NAME = "RN50"
+
+CACHE_PATH = os.path.join(SCRIPT_DIR, "all_features.pt")
 
 def main():
     parser = argparse.ArgumentParser(
@@ -23,29 +26,82 @@ def main():
         type=str,
         help="path of photos folder",
     )
+    parser.add_argument(
+        "-t",
+        "--target",
+        type=str,
+        help="target image to search for neighbors of",
+    )
     print(f"available models:")
     print(clip.available_models())
     args = parser.parse_args()
 
     assert os.path.isdir(args.input_dir)
     img_paths = find_images(args.input_dir)
+    assert os.path.isfile(args.target)
+    args.target = os.path.abspath(args.target)
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model, preprocess = clip.load(MODEL_NAME, device=device)
+    #img_paths = img_paths[:20] # for now
+    #target_path = img_paths[0]
     with torch.no_grad():
-        # target_path = img_paths[0]
         dur = 0
-        all_features: list[torch.Tensor] = []
+        all_features: dict[str, torch.Tensor] = dict()
+
+        def encode_image(fname: str) -> torch.Tensor:
+            image = preprocess(Image.open(fname)).unsqueeze(0).to(device)
+            features = model.encode_image(image)
+            features /= features.norm(dim=-1, keepdim=True)
+            return features
+
+
+        # target_features = encode_image(args.target)
         for i, img_path in enumerate(img_paths):
-            image = preprocess(Image.open(img_path)).unsqueeze(0).to(device)
             start_time = time.perf_counter()
-            img_features = model.encode_image(image)
-            img_features /= img_features.norm(dim=-1, keepdim=True)
+            img_features = encode_image(img_path)
             dur += time.perf_counter() - start_time
 
-            all_features.append(img_features)
+            all_features[os.path.basename(img_path)] = img_features
             if i % 5 == 0:
                 print(f"at image {i}/{len(img_paths)}, averaging {(dur/len(all_features)):.4f} sec/image")
+    
+        torch.save(all_features, CACHE_PATH)
+        print(f"wrote '{CACHE_PATH}'")
+        x = torch.stack(list(all_features.values())).to(device)
+
+        def query_similar(fname: str) -> str:
+            """Given an image, return the name of the most similar image."""
+            if fname in all_features:
+                target_features = all_features[fname]
+            else:
+                target_features = encode_image(fname)
+
+            sims =  (x @ target_features.T).squeeze(1)  # Compute cosine similarity
+            print(f"{sims.shape=}")
+            top5_values, top5_indices = sims.topk(5, largest=True, axis=0)
+
+            k_idx_map = dict()
+            i = 0
+            for name in all_features.keys():
+                k_idx_map[i] = name
+                i += 1
+
+            print(f"\n\nmost similar images to {fname}:")
+            # get matching filenames
+            matches = [k_idx_map[i] for i in top5_indices.flatten().tolist()]
+            print("\n".join(matches))
+        
+        query_similar(args.target)
+        while True:
+            target = input(f"image path > ").strip()
+            if not os.path.isabs(target):
+                target = os.path.join(args.input_dir, target)
+
+            try:
+                query_similar(target)
+            except FileNotFoundError:
+                print(f"file not found")
 
 
 def find_images(input_dir: str) -> list[str]:
@@ -57,10 +113,10 @@ def find_images(input_dir: str) -> list[str]:
         ext = os.path.splitext(fpath)[1].lstrip(".")
         if ext.lower() not in SUPPORTED_EXTS:
             continue
-        img_paths.append(fpath)
+        img_paths.append(os.path.abspath(fpath))
     
     img_paths = sorted(img_paths)
-    print(f"collected {len(img_paths)} images")
+    print(f"found {len(img_paths)} images")
     print(f"'{img_paths[0]}' -> '{img_paths[-1]}'")
     return img_paths
 
