a4552d1d8aa7e70c05132e9651a1bc177f67e9f8
Daniel Engbert <end1@umbc.edu>
2025-11-04 00:10:30 +0100

    add error handling

--- go/greetings/greetings.go
+++ go/greetings/greetings.go
@@ -1,9 +1,16 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if (name == "") {
+		return "", errors.New("you provided an empty name my guy")
+	}
+
 	// ':=' simultaneously declares and intializes a var
 	msg := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return msg
+	return msg, nil
 }
--- go/hello/hello.go
+++ go/hello/hello.go
@@ -1,16 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	// https://pkg.go.dev/rsc.io/quote@v1.5.2
+	"rsc.io/quote"
+	// my module
+	"example.com/greetings"
+)
 
-// https://pkg.go.dev/rsc.io/quote@v1.5.2
-import "rsc.io/quote"
-
-import "example.com/greetings"
 
 func main() {
+	log.SetPrefix("[greetings]: ")
+	log.SetFlags(0)
+
 	fmt.Println("what's up")
 	fmt.Println(quote.Opt())
 
-	msg := greetings.Hello("jullie")
+	msg, err := greetings.Hello("jullie")
+	//fmt.Printf("msg: '%v', err: '%v'\n", msg, err)
 	fmt.Println(msg)
+
+	msg, err = greetings.Hello("")
+	fmt.Println(msg)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
